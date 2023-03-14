e652f2488f8ebe3311e8bb28a5f6cb5128636261
Marvin Elsen <development@marvin-elsen.com>
2023-03-14 20:38:47 +0800

    Add logger

--- src/main/kotlin/com/marvin_elsen/better_snow/BetterSnowMod.kt
+++ src/main/kotlin/com/marvin_elsen/better_snow/BetterSnowMod.kt
@@ -1,7 +1,12 @@
 package com.marvin_elsen.better_snow
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 const val MOD_ID = "better-now"
+val LOGGER: Logger = LoggerFactory.getLogger(MOD_ID)
 
+@Suppress("unused")
 fun init() {
-
+    LOGGER.info("Initializing Milky's Mod")
 }
