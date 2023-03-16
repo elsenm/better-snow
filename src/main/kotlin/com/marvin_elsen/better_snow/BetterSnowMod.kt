84fae772cd9e0b1fc7370e3f364c47ecc211218c
Marvin Elsen <development@marvin-elsen.com>
2023-03-16 18:56:59 +0800

    Replace hyphen in modid with underscore

--- src/main/kotlin/com/marvin_elsen/better_snow/BetterSnowMod.kt
+++ src/main/kotlin/com/marvin_elsen/better_snow/BetterSnowMod.kt
@@ -3,7 +3,7 @@ package com.marvin_elsen.better_snow
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-const val MOD_ID = "better-now"
+const val MOD_ID = "better_snow"
 val LOGGER: Logger = LoggerFactory.getLogger(MOD_ID)
 
 @Suppress("unused")
