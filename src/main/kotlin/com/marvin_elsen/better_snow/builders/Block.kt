1a6821382a91fff0fc671955fbdcb60f10920f34
Marvin Elsen <development@marvin-elsen.com>
2023-03-17 14:39:11 +0800

    Implement stairs type for block creation

--- src/main/kotlin/com/marvin_elsen/better_snow/builders/Block.kt
+++ src/main/kotlin/com/marvin_elsen/better_snow/builders/Block.kt
@@ -3,6 +3,7 @@ package com.marvin_elsen.better_snow.builders
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.minecraft.block.BlockState
 import net.minecraft.item.BlockItem
 import net.minecraft.item.ItemGroup
 import net.minecraft.registry.Registries
@@ -13,6 +14,7 @@ class Block {
     lateinit var identifier: Identifier
     lateinit var itemGroup: ItemGroup
     lateinit var settings: FabricBlockSettings
+    lateinit var blockState: BlockState
     var type: Type = Type.NORMAL
 
     fun identifier(identifier: Identifier) {
@@ -31,6 +33,10 @@ class Block {
         this.type = type
     }
 
+    fun blockState(blockState: BlockState) {
+        this.blockState = blockState
+    }
+
     enum class Type {
         NORMAL, SLAB, STAIR
     }
@@ -43,7 +49,7 @@ fun block(init: Block.() -> Unit): net.minecraft.block.Block {
     val minecraftBlock = when (block.type) {
         Block.Type.NORMAL -> net.minecraft.block.Block(block.settings)
         Block.Type.SLAB -> net.minecraft.block.SlabBlock(block.settings)
-        Block.Type.STAIR -> TODO()
+        Block.Type.STAIR -> net.minecraft.block.StairsBlock(block.blockState, block.settings)
     }
 
     val registeredBlock = Registry.register(Registries.BLOCK, block.identifier, minecraftBlock)
