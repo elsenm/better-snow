c5d99e8cbc9f49d3903a0344f3fc739035d41700
Marvin Elsen <development@marvin-elsen.com>
2023-03-17 14:01:19 +0800

    Add packed snow slab

--- src/main/kotlin/com/marvin_elsen/better_snow/BetterSnowMod.kt
+++ src/main/kotlin/com/marvin_elsen/better_snow/BetterSnowMod.kt
@@ -1,5 +1,6 @@
 package com.marvin_elsen.better_snow
 
+import com.marvin_elsen.better_snow.builders.Block
 import com.marvin_elsen.better_snow.builders.block
 import net.minecraft.block.Material
 import net.minecraft.item.ItemGroups
@@ -23,6 +24,18 @@ fun initBlocks() {
 
     block {
         identifier(BetterSnowIdentifier("packed_snow_block"))
+        settings {
+            material(Material.SNOW_BLOCK)
+            strength(0.5f)
+            requiresTool()
+            sounds(BlockSoundGroup.SNOW)
+        }
+        itemGroup(ItemGroups.NATURAL)
+    }
+
+    block {
+        identifier(BetterSnowIdentifier("packed_snow_slab"))
+        type(Block.Type.SLAB)
         settings {
             material(Material.SNOW_BLOCK)
             strength(0.5f)
