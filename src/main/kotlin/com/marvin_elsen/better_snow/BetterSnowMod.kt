b9dccacd7d4e3e163ac46ed1a461b072c18198cc
Marvin Elsen <development@marvin-elsen.com>
2023-03-17 14:40:09 +0800

    Add packed snow stairs

--- src/main/kotlin/com/marvin_elsen/better_snow/BetterSnowMod.kt
+++ src/main/kotlin/com/marvin_elsen/better_snow/BetterSnowMod.kt
@@ -2,6 +2,7 @@ package com.marvin_elsen.better_snow
 
 import com.marvin_elsen.better_snow.builders.Block
 import com.marvin_elsen.better_snow.builders.block
+import net.minecraft.block.Blocks
 import net.minecraft.block.Material
 import net.minecraft.item.ItemGroups
 import net.minecraft.sound.BlockSoundGroup
@@ -36,6 +37,19 @@ fun initBlocks() {
     block {
         identifier(BetterSnowIdentifier("packed_snow_slab"))
         type(Block.Type.SLAB)
+        settings {
+            material(Material.SNOW_BLOCK)
+            strength(0.3f)
+            requiresTool()
+            sounds(BlockSoundGroup.SNOW)
+        }
+        itemGroup(ItemGroups.NATURAL)
+    }
+
+    block {
+        identifier(BetterSnowIdentifier("packed_snow_stairs"))
+        type(Block.Type.STAIRS)
+        blockState(Blocks.SNOW_BLOCK.defaultState)
         settings {
             material(Material.SNOW_BLOCK)
             strength(0.3f)
